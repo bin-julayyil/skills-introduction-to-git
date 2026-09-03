19891c738585c7cd055eb922697261d8d580798f
abdullah bin julayyil <262361715+bin-julayyil@users.noreply.github.com>
2026-09-03 09:26:58 +0300

    Initialize release/v0.17.1-pr-13428

---
+++ packages/cli/src/ui/components/AppHeader.tsx
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Box } from 'ink';
+import { Header } from './Header.js';
+import { Tips } from './Tips.js';
+import { useSettings } from '../contexts/SettingsContext.js';
+import { useConfig } from '../contexts/ConfigContext.js';
+import { useUIState } from '../contexts/UIStateContext.js';
+import { Banner } from './Banner.js';
+import { useBanner } from '../hooks/useBanner.js';
+import { theme } from '../semantic-colors.js';
+
+interface AppHeaderProps {
+  version: string;
+}
+
+export const AppHeader = ({ version }: AppHeaderProps) => {
+  const settings = useSettings();
+  const config = useConfig();
+  const { nightly, mainAreaWidth, bannerData, bannerVisible } = useUIState();
+
+  const { bannerText } = useBanner(bannerData, config);
+
+  return (
+    <Box flexDirection="column">
+      {!(settings.merged.ui?.hideBanner || config.getScreenReader()) && (
+        <>
+          <Header version={version} nightly={nightly} />
+          {bannerVisible && bannerText && (
+            <Banner
+              width={mainAreaWidth}
+              bannerText={bannerText}
+              color={
+                bannerData.warningText === ''
+                  ? theme.border.default
+                  : theme.status.warning
+              }
+            />
+          )}
+        </>
+      )}
+      {!(settings.merged.ui?.hideTips || config.getScreenReader()) && (
+        <Tips config={config} />
+      )}
+    </Box>
+  );
+};
