dc4546fdfad74841c803421e1796d1668a59438c
abdullah bin julayyil <262361715+bin-julayyil@users.noreply.github.com>
2026-09-03 10:08:27 +0300

    Initialize st/forever

---
+++ packages/cli/src/utils/events.ts
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { EventEmitter } from 'node:events';
+
+export enum TransientMessageType {
+  Warning = 'warning',
+  Hint = 'hint',
+}
+
+export interface TransientMessagePayload {
+  message: string;
+  type: TransientMessageType;
+}
+
+export enum AppEvent {
+  OpenDebugConsole = 'open-debug-console',
+  Flicker = 'flicker',
+  SelectionWarning = 'selection-warning',
+  PasteTimeout = 'paste-timeout',
+  TerminalBackground = 'terminal-background',
+  TransientMessage = 'transient-message',
+  ExternalMessage = 'external-message',
+  A2AListenerStarted = 'a2a-listener-started',
+}
+
+export interface AppEvents {
+  [AppEvent.OpenDebugConsole]: never[];
+  [AppEvent.Flicker]: never[];
+  [AppEvent.SelectionWarning]: never[];
+  [AppEvent.PasteTimeout]: never[];
+  [AppEvent.TerminalBackground]: [string];
+  [AppEvent.TransientMessage]: [TransientMessagePayload];
+  [AppEvent.ExternalMessage]: [string];
+  [AppEvent.A2AListenerStarted]: [number];
+}
+
+export const appEvents = new EventEmitter<AppEvents>();
